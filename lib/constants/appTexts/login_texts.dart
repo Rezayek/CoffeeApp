0036173cf6114b237b91a06f0ec635ceda581d16
Abderrazek Souguir <yomyfriendhh@gmail.com>
2022-03-11 19:24:44 +0100

    Initial commit

---
+++ lib/constants/appTexts/login_texts.dart
@@ -0,0 +1,5 @@
+const String welcomeText = 'Welcome to MyCoffee';
+const String loginTextBtn = 'Login';
+const String registerTextBtn = 'Register';
+const String forgetPasswordText = 'forgot your password ?';
+const String clicKHereText = 'click here';
---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color coffeeColor = Color(0x006f4e37);
+const Color blackCoffeeColor = Color(0x003b302f);
+const Color brownCoffeeColor = Color(0x004a2c2a);
+const Color orangeCoffeeColor = Color(0x00883300);
+const Color coldBrewCoffeeColor = Color(0x00785736);
+const Color freshlyRoastedCoffeeColor = Color(0x00663322);
+const Color irishCoffeeColor = Color(0x0062422b);
+const Color coffeeCakeColor = Color(0x00eee9d3);
+const Color arabicCoffeeColor = Color(0x006f4d3f);
+const Color oldCoffeeColor = Color(0x00704241);
